4af8c4d67214eb07423a6e8230792e6f1ab70f82
AlexTheseus <631920759@qq.com>
2017-01-08 10:05:16 +0800

    leftBar

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,14 @@
 $(function () {
     $('#menu').click(function () {
-        $('header').animate({left:'220px'});
-        $('.leftBar1').animate({width:'220px'});
-        $('#menu').addClass('menuCross');
-
+        if($('#menu').hasClass('menuCross')){
+            $('#menu').removeClass('menuCross');
+            $('header').animate({left: '0'});
+            $('.leftBar1').animate({width: '0'});
+        }
+        else{
+            $('header').animate({left: '220px'});
+            $('.leftBar1').animate({width: '220px'});
+            $('#menu').addClass('menuCross');
+        }
     })
 })
